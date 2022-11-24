7094e230912aee468b2fcb554ba796402c3154bd
julieheim <heimjulie78@gmail.com>
2022-11-24 18:17:19 +0000

    ViewBinding beginning

--- app/src/main/java/com/example/personnal_project_heim/MainActivity.kt
+++ app/src/main/java/com/example/personnal_project_heim/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.personnal_project_heim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.personnal_project_heim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = binding.root
+        setContentView(rootView)
+       // setContentView(R.layout.activity_main)
+
+      //  val helloWorldTextView = findViewById<TextView>(R.id.hello_word_text_view)
+        Log.i("MainActivity", binding.helloWordTextView.text.toString())
     }
 }
